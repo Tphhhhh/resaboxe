52426c6b38dedc4f00a20266fc1be841fbe11127
Tphhhhh <theoto92@yahoo.fr>
2025-07-01 15:37:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render impose PORT
+    app.run(host="0.0.0.0", port=port)
