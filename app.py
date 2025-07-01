71bc35f00e4d7e415ef11f38d6ac3195ddab63db
Tphhhhh <theoto92@yahoo.fr>
2025-07-01 15:18:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def index():
 
         return "Surveillance lancée avec succès !"
 
-    return render_template("form.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
